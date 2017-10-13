d792154d30ea55ec49726834e1a0f427dd6a57b6
Shin Kudo <ShinKudo@SKs-MacBook-Pro.local>
2017-10-13 14:57:57 +0900

    配列

--- lib/bowling2.rb
+++ lib/bowling2.rb
@@ -1,17 +1,17 @@
 class Bowling
     def initialize
         @total_score = 0
-        @frame_score = 0
+        @frame_score = Array.new(10,0)
         @bowl_count = 0
-        @frame_count = 1
+        @frame_count = 0
         @spare_flg = 0
     end
     
     def add_score(pins)
-        @frame_score += pins
+        @frame_score[@frame_count] += pins
 
         if @spare_flg == 1
-            @total_score += pins
+            @frame_score[@frame_count - 1] += pins
             @spare_flg = 0
         end
 
@@ -22,7 +22,6 @@ class Bowling
         @bowl_count +=1
 
         if @bowl_count == 2
-            calc_score
             @bowl_count = 0
             @frame_score = 0
             @frame_count += 1
@@ -33,6 +32,8 @@ class Bowling
         @total_score
     end
     def calc_score
-        @total_score += @frame_score
+        @framescore.each {|score|
+            @total_score += score
+        }
     end
 end
